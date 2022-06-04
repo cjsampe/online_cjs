ca90629df8b41b08ac1d665fe364823ca5109654
cjsampe <samperiz@hotmail.com>
2022-06-04 06:52:35 +0200

    subiendo la primera version

---
+++ src/main/java/com/online/online/service/ProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.online.online.service;
+
+import com.online.online.domain.Product;
+import com.online.online.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+}
